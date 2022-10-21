175d981be7580c704de33197bfa59a6b6cbcdd2f
Conner Schiissler <coschiissler@suncor.com>
2022-10-21 10:57:31 -0600

    adding wsl install

--- .config/powershell/install_apps.ps1
+++ .config/powershell/install_apps.ps1
@@ -26,4 +26,5 @@ Install-Module posh-git -scope CurrentUser -Force
 Install-Module -Name Terminal-Icons -Repository PSGallery -Force 
 Install-Module -Name z -Force
 Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
-Install-Module -Name PSFzf -Scope CurrentUser -Force 
+Install-Module -Name PSFzf -Scope CurrentUser -Force
+wsl --install
